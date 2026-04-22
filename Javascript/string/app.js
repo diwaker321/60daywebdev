af5f79f7fcb73e5e7a8d38f81a187d078562ba7e
diwaker321 <diwakeracadmic@gmail.com>
2026-04-22 11:02:25 +0530

    string has beed pushed

---
+++ Javascript/string/app.js
@@ -0,0 +1,104 @@
+// console.log("hello world");
+// let val = 25;
+// if(val>20){
+//     console.log("if case is run");
+// }else{
+//     console.log("else case is run");    
+// }
+// ternary operatior 
+// (val>20) ? console.log("if case is run") : console.log("else case is run");
+
+//type of
+
+// console.log(typeof val);
+// console.log(typeof val);
+// val={}
+// console.log(typeof val);
+// val=[]
+// console.log(typeof val);
+// console.log(typeof val);
+
+// val="name"
+// val = new String("name")
+
+
+// let a =10;
+let a ="10";
+// console.log('a: ',typeof a);
+let c =10;
+// console.log('c: ',typeof c);
+
+
+let b = 20;
+// console.log(a+c); // it gives 1010
+// console.log(a-c); // it gives 0
+// console.log(a/c); // it gives 1
+// let name1 = "diwakersdfsdkfskdfmskdmskdfskdmvksdmskdfmwkgsdkmvaksmdfakfclssadasdsdgdfdfgsdfsfsdfsdf"
+let name1="diwaker hello good morning"
+// console.log('name1: ', name1.);
+// console.log('name1: ', name1);
+let name2="              dwivedi             "
+// console.log('name2: ', name2);
+// const classname = 'diwaker "dwivedi"'
+// const classname = "diwaker 'dwivedi'"
+const classname = 'diwaker \'dwivedi\' '
+
+
+console.log('classname: ', classname);
+let finalversion = name2.trim()
+// console.log('finalversion: ', finalversion.length);
+// console.log('name2: ', name2);
+// console.log('name2: ', name2.trim());
+let atmpin = "1224********"
+let aadharCard =512656334423435
+let updatedAdharCard = "5126"
+// console.log('updatedAdharCard: ', updatedAdharCard.padStart(16,"*"));
+// console.log('updatedAdharCard: ', updatedAdharCard.padEnd(16,"*"));
+// console.log(name1.indexOf());
+// console.log(name1.slice(8,2)); // 0 se 12th
+// console.log(name1.substring(8,2)); // 0 se 12th
+
+
+
+
+
+
+// console.log('aadharCard: ', aadharCard);
+let email = "diwakergmail.com"
+// console.log(name1.length);
+// console.log(name1[85]);
+// console.log(name1[name1.length-1]);
+// console.log(name1.at(7));
+// console.log(name1.charAt(7));
+// console.log(name1.endsWith('1'));
+// console.log(atmpin.startsWith('*'));
+// console.log(name1.split(" "));
+// console.log(email.includes("@"));
+// console.log(name1.);
+
+
+
+
+
+
+
+
+
+
+
+// console.log(name1+ " "+name2)
+// console.log(name1.concat(" ",name2));
+// console.log(`${name1} ${name2}`);
+// console.log(`${2+2} ${name2}`);
+
+// console.log(`${name1 name2}`);
+
+
+
+
+
+// console.log(c===a);
+
+
+
+
