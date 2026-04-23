f58fe648f1e9be4f172a6cc885b2e46520dc38df
diwaker321 <diwakeracadmic@gmail.com>
2026-04-23 11:35:24 +0530

    array has been started

---
+++ Javascript/Array/app.js
@@ -0,0 +1,95 @@
+// console.log("hello array");
+
+// let names = ["diw" , "dwi" , 1921059 , "diwaker@gmail.com" , "001" , {} , "sarthak" , "chug" , "aaliya" , "saifi"]
+// console.log('names: ', names[names.length-2]);
+// console.log('names.length: ', names.length);
+
+// for(let i = 0 ; i<=names.length-1;i++){
+//     console.log(names[i]);
+
+// }
+
+// let arr = new Array("diw" , "dwi" , 1921059)
+// let arr3 = new Array("10")
+// console.log('arr3: ', arr3);
+// let arr2=[10,20]
+// console.log('arr2: ', arr2);
+// console.log('arr: ', arr);
+
+// let arr = [];
+// arr[0] = "diw";
+// arr[1] = "diw";
+// arr[2] = "diw";
+// arr[3] = "diw";
+// arr[4] = "diw";
+// arr[5] = "diw";
+// arr[6] = "diw";
+// console.log('arr.length: ', arr.length);
+
+// arr[arr.length] = "sarthak"
+// arr.length
+// console.log('arr.length: ', arr.length);
+// arr.push("sarthak")
+// let remove_val = arr.pop()
+// console.log('remove_val: ', remove_val);
+// console.log("arr: ", arr);
+// arr[7] = "sarthak"
+
+// const names = ["diwaker"]
+// names=["dwivedi" , "001"]
+// names[0]="dwivedi dwivedi"
+// names[1]="diwaker"
+
+// console.log('names: ', names);
+
+//sorting
+
+let chararr = ["C", "A", "E", "F", "B"];
+// let numarr = [ 5,4,3,2,1] //5,4,3,2,1
+let numarr2 = [5, 4, 3, 20, 10]; //20,10,5,4,3
+// console.log('numarr: ', numarr2.sort((a,b)=> a-b));
+// console.log('numarr: ', numarr.sort((a,b)=> a-b));
+// let result = [...numarr ,  ...numarr2]
+// let result = numarr.concat(numarr2)
+// console.log('result: ', result);
+// console.log('result: ', result);
+// console.log('result: ', result.sort((a,b)=>a-b));
+// let newarr3= {...numarr}
+// let newarr = [numarr]
+// let newarr2 = [...numarr]
+
+// console.log('newarr: ', newarr);
+// console.log('newarr2: ', newarr2);
+// console.log('newarr2: ', newarr3);
+
+// let numarr = [5, 4, 3, 2, 1]; //5,4,3,2,1
+
+// let newval = [[numarr]];
+// console.log("newval: ", newval.flat(10).toString());
+// console.log('newval: ', newval.toString());
+
+
+// console.log('numarr: ', numarr2.sort());
+
+// console.log('chararr: ', chararr.sort());
+
+
+//arrayfrom method
+
+let text = "diwaker";
+// let arr = text.split("")
+let arr = Array.from(text)
+
+console.log('arr: ', arr);
+arr[0] = 'a'
+console.log('arr: ', arr.toString());
+
+// text[0] = 'a'
+// console.log('text[0]: ', text);
+// let arr =Array.from(text)
+// arr.pop()
+// console.log('arr: ', arr.toString());
+// console.log(Array.from(text).pop());
+// console.log(text.split(""));
+
+
