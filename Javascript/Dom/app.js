9d084dba6ef30d1a8d4f315484d183c82201e259
diwaker321 <diwakeracadmic@gmail.com>
2026-04-29 12:30:58 +0530

    dom concept has been completed

--- Javascript/Dom/app.js
+++ Javascript/Dom/app.js
@@ -1,17 +1,18 @@
 // intro of Dom 
+
 //how to select element -by id , class , by tag
 //queryselector and all.
 //how to change content -innerhtml,text,content
-//how to change attributes of element using direct method 
+//how to change attributes of element. 
 {/* <img id="pic" src="old.jpg" width="200"></img> */}
     // img.setAttribute("attributeName", "value") , getattribute ("attubute name")
     //exercise - input placeholder change  , anchor link change , 
 //how to change css
-//how to add class-element.classlist.add()
+//how to add class-element.classlist.add() and .remove("forRemoveing")
     //<p id="text">Hello</p>
 
 //how to make new element - using document.createelement()
-//how to append and removechild using 
+//how to append and remove  child using 
 //Parent / Child / Sibling
 // element.parentElement
 // element.children
